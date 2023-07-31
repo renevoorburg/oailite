e7afaeea6819b8de87757e44d3e461ce1ec352cb
renevoorburg <voorburg@xs4all.nl>
2023-07-31 17:18:40 +0200

    enhanced dbwalker

--- dbwalker.sh
+++ dbwalker.sh
@@ -2,34 +2,29 @@
 
 # dbwalker.sh
 # part of https://github.com/renevoorburg/oailite
-# 2023-05-21
 
 set +H
 
-export DEST_DB
-export DEST_TABLE
-export RECORD_ID
-
 read_commandline_parameters()
 {
-    PROG=$0
-    SCRIPT_DIR=$(dirname "$PROG")
+    prog=$0
+    script_dir=$(dirname "$prog")
     while getopts "hd:t:f:u:p:s:" option ; do
         case $option in
             h)  usage
                 ;;
-            s)  SOURCE_DB="$OPTARG"
+            s)  source_db="$OPTARG"
                 ;;
-            t)  TABLE="$OPTARG"
-                DEST_TABLE="$TABLE"
+            t)  table="$OPTARG"
+                dest_table="$table"
                 ;;
-            f)  FROM="$OPTARG"
+            f)  from="$OPTARG"
                 ;;
-            u)  UNTIL="$OPTARG"
+            u)  until="$OPTARG"
                 ;;
-            p)  FILTER="$SCRIPT_DIR/$OPTARG"
+            p)  processor="$script_dir/$OPTARG"
                 ;;
-            d)  DEST_DB=`pwd`"/$OPTARG"
+            d)  dest_db="$OPTARG"
                 ;;
             ?)  usage
                 ;;
@@ -42,7 +37,7 @@ usage()
 {
     cat << EOF
 
-usage: $PROG [OPTIONS] -s [source db] -t [source table]
+usage: $prog [OPTIONS] -s [source db] -t [source table]
 
 This is a helper script for processing data stored in an sqlite database, as retrieved by oailite.sh. 
 It returns the selected captured payload for further processing.
@@ -59,13 +54,13 @@ OPTIONS:
 EXAMPLES:
 
 Return all payload from GGC-THES.db, table mdoall:
-$PROG -s GGC-THES.db -t mdoall
+$prog -s GGC-THES.db -t mdoall
 
 Return selected payload and process it with ./nta2schema.sh:
-$PROG -s GGC-THES.db -t mdoall -f "2023-05-15 17:56:03" -p "nta2schema.sh -x"
+$prog -s GGC-THES.db -t mdoall -f "2023-05-15 17:56:03" -p "nta2schema.sh -x"
 
 Return selected payload, process it with ./nta2schema.sh and store it in OUT.db:
-$PROG -s GGC-THES.db -t mdoall -f "2023-05-15 17:56:03" -p "nta2schema.sh -x" -d OUT.db
+$prog -s GGC-THES.db -t mdoall -f "2023-05-15 17:56:03" -p "nta2schema.sh -x" -d OUT.db
 
 EOF
     exit
@@ -81,66 +76,93 @@ check_software_dependencies()
 
 set_valid_initial_parameters()
 {
-    if [ -z "$SOURCE_DB" ] ; then
+    if [ -z "$source_db" ] ; then
         echo "A source database (-s) must be specified. "
         usage
     fi
-    if [ ! -f "$SOURCE_DB" ] ; then
-        echo "Database $SOURCE_DB not found."
+    if [ ! -f "$source_db" ] ; then
+        echo "Database $source_db not found."
         exit
     fi
-    if [ -z "$TABLE" ] ; then
+    if [ -z "$table" ] ; then
         echo "A table (-t) in the source database must be specified."
         usage
     fi 
-    if [ `sqlite3 -batch $SOURCE_DB "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='$TABLE';"` -eq 0 ] ; then
-        echo "Table $TABLE does not exist."
+    if [ `sqlite3 -batch $source_db "SELECT count(*) from sqlite_master WHERE type='table' AND name='$table';"` -eq 0 ] ; then
+        echo "Table $table does not exist."
         exit 1
-    fi
-    if [ -z "$DEST_DB" ] ; then
-        PROCESSOR="cat" 
-    else
-        PROCESSOR="$SCRIPT_DIR/dbstore.sh"
-    fi
-    if [ -z "$FILTER" ] ; then
-        FILTER="cat" 
+    fi    
+    if [ -z "$processor" ] ; then
+        processor="cat" 
     fi
 
-    RESTRICT_SQL=''
-    if [  ! -z "$FROM" ] ; then
-        RESTRICT_SQL="$RESTRICT_SQL AND timestamp >= '$FROM'"
+    restrict_sql=''
+    if [ ! -z "$from" ] ; then
+        restrict_sql="$restrict_sql AND timestamp >= '$from'"
     fi 
-    if [ ! -z "$UNTIL" ] ; then 
-        RESTRICT_SQL="$RESTRICT_SQL AND timestamp <= '$UNTIL'"
+    if [ ! -z "$until" ] ; then 
+        restrict_sql="$restrict_sql AND timestamp <= '$until'"
     fi
 
-    ROWCOUNT=10000
-    OFFSET=0
-    COUNTER=0
-    NUMRECS=`sqlite3 -batch $SOURCE_DB "SELECT count(id) FROM $TABLE WHERE 1 $RESTRICT_SQL"`
+    rowcount=10000
+    offset=0
+    counter=0
+    numrecs=`sqlite3 -batch $source_db "SELECT count(id) from $table WHERE 1 $restrict_sql"`
 }
 
 prepare_destination_database()
 {
-    if [ ! -z "$DEST_DB" ] ; then
-        if [ ! -f "$DEST_DB" ] ; then
-            sqlite3 -batch $DEST_DB "create table $DEST_TABLE (id TEXT PRIMARY KEY, timestamp TEXT, sourcedata TEXT);"
+    writer="cat" 
+    if [ ! -z "$dest_db" ] ; then
+        if [ ! -f "$dest_db" ] ; then
+            writer="create_record"
+            sqlite3 -batch $dest_db "create table $dest_table (id TEXT PRIMARY KEY, timestamp TEXT, sourcedata TEXT);"
             if [ $? -ne 0 ] ; then
-                echo "An error occured when creating destination database $DEST_DB, table $DEST_TABLE."
+                echo "An error occured when creating destination database $dest_db, table $dest_table."
                 exit 1
             else
-                if [ `sqlite3 -batch $DEST_DB "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='$DEST_TABLE';"` -eq 0 ] ; then
-                    sqlite3 -batch $DEST_DB "create table $DEST_TABLE (id TEXT PRIMARY KEY, timestamp TEXT, sourcedata TEXT);"
+                if [ `sqlite3 -batch $dest_db "SELECT count(*) from sqlite_master WHERE type='table' AND name='$dest_table';"` -eq 0 ] ; then
+                    sqlite3 -batch $dest_db "create table $dest_table (id TEXT PRIMARY KEY, timestamp TEXT, sourcedata TEXT);"
                 fi
             fi
+        elif  [ "$dest_db" = "$source_db" ] ; then
+            writer="update_record"
+            # make sure we have columns for the processed data:
+            sqlite3 -batch $dest_db "alter table $dest_table add column processed TEXT;" 2> /dev/null
+            sqlite3 -batch $dest_db "alter table $dest_table add column processor TEXT;" 2> /dev/null
+        else
+            writer="create_record"
         fi
+    fi
+}
+
+create_record()
+{
+    piped_data=`cat <&0`
+    if [ ! -z "$piped_data" ] ; then
+        sqlite3 -batch $dest_db "pragma busy_timeout=20000; 
+            REPLACE INTO $dest_table (id, timestamp, sourcedata) 
+            VALUES ('$record_id', datetime(), '$(echo "$piped_data" | perl -pe "s@'@''@g")') ;" > /dev/null
+    fi
+}
+
+update_record() 
+{
+    piped_data=`cat <&0`
+    if [ ! -z "$piped_data" ] ; then
+        sqlite3 -batch $dest_db "pragma busy_timeout=20000; 
+            UPDATE $dest_table 
+            SET timestamp = datetime(), 
+                processed = '$(echo "$piped_data" | perl -pe "s@'@''@g")', 
+                processor = '${processor}' 
+            WHERE id = '$record_id' ;" > /dev/null
     fi
 }
 
 process_record()
 {
-    RECORD_ID=$1
-    echo "`sqlite3 -batch $SOURCE_DB "SELECT sourcedata FROM $TABLE WHERE id='$RECORD_ID'"`" | $FILTER | "$PROCESSOR"
+    record_id=$1
+    echo "`sqlite3 -batch $source_db "SELECT sourcedata from $table WHERE id='$record_id'"`" | $processor | "$writer"
 }
 
 read_commandline_parameters "$@"
@@ -148,10 +170,10 @@ check_software_dependencies
 set_valid_initial_parameters
 prepare_destination_database
 
-until [ $COUNTER -ge $NUMRECS ] ; do
+until [ $counter -ge $numrecs ] ; do
     while read id ; do
         process_record $id
-       ((COUNTER++))
-    done <<< "$(sqlite3 -batch $SOURCE_DB "SELECT id FROM $TABLE WHERE 1 $RESTRICT_SQL ORDER BY timestamp LIMIT $OFFSET, $ROWCOUNT")"
-    OFFSET=$((OFFSET+ROWCOUNT))
+       ((counter++))
+    done <<< "$(sqlite3 -batch $source_db "SELECT id from $table WHERE 1 $restrict_sql ORDER BY timestamp LIMIT $offset, $rowcount")"
+    offset=$((offset+rowcount))
 done
